39f14d42b9d14e7a1dfeb160ccb6f9325f544cf8
Muhammad_Irfan_Efendi_122140013 <114717942+MuhammadIIIrfan@users.noreply.github.com>
2025-05-19 08:55:34 +0000

    commit11

--- frontend/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: "http://localhost:6543/api", // Ganti sesuai backend Pyramid kamu
-});
-
-export const getBerita = () => API.get("/berita");
-export const createBerita = (data) => API.post("/berita", data);
-// Tambah update, delete, dll.
